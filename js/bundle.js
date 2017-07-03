a07f826f7a10332132efc409347bec7a7f640dbf
Cherry Lam <siuding0601@gmail.com>
2017-07-03 16:22:12 -0700

    Implement game with game logic and views

--- js/bundle.js
+++ js/bundle.js
@@ -105,23 +105,40 @@ class View {
 
   bindEvents() {
     this.$el.find("li").each( (idx, $li) => {
-      $li.click( () => {});
+      $li.click( () => {
+          // this.game.playMove($li.data('pos'));
+          this.makeMove($li);
+        });
     });
   }
 
-  makeMove($square) {}
+  makeMove($square) {
+    const mark = this.game.currentPlayer;
+    $square.text(mark);
+    if (mark === "x") {
+      $square.addClass('x');
+    } else{
+      $square.addClass('o');
+    }
+    if (!$square.text()) {
+      alert(`Wrong Move`);
+    }
+    this.game.playMove($square.data('pos'));
+  }
 
   setupBoard() {
     const $ul = $('<ul></ul>');
     this.$el.append($ul);
-    for (let i = 0; i < 9; i++) {
-      const $li = $('<li></li>');
-      $ul.append($li);
-      // $li.on("hover", () => {
-      //   // console.log($li);
-      //   $li.addClass("selected");
-      // });
-
+    // for (let i = 0; i < 9; i++) {
+    //   const $li = $('<li></li>');
+    //   $ul.append($li);
+    // }
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const $li = $('<li></li>');
+        $li.data('pos', [i, j]);
+        $ul.append($li);
+      }
     }
   }
 }
@@ -155,7 +172,7 @@ class Game {
     const game = this;
 
     this.board.print();
-    console.log(`Current Turn: ${this.currentPlayer}`)
+    console.log(`Current Turn: ${this.currentPlayer}`);
 
     reader.question('Enter rowIdx: ', rowIdxStr => {
       const rowIdx = parseInt(rowIdxStr);
